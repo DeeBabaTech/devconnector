239681d11d41f86c334e70039c6137fb2b281c65
DeeBabaTech <dee1dayo4real@gmail.com>
2023-12-28 17:22:52 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
 
 // Init Middleware
+app.use(cors());
 app.use(express.json());
+// app.use(serializableCheck({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
 
